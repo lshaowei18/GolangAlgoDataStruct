adad25277916b63c431595505d6ac9af5b0ee5aa
Shao Wei <lshaowei18@gmail.com>
2019-03-20 22:34:50 +0800

    Added enqueue and swap

--- priorityqueue/pq.go
+++ priorityqueue/pq.go
@@ -8,3 +8,24 @@ type Node struct {
 type PriorityQueue struct {
 	nodes []Node
 }
+
+func (pq *PriorityQueue) Enqueue(priority int, value string) {
+	node := Node{priority, value}
+	pq.nodes = append(pq.nodes, node)
+
+	index := len(pq.nodes) - 1
+	parentIndex := (index - 1) / 2
+
+	for pq.nodes[index].priority < pq.nodes[parentIndex].priority {
+		//Swap Values
+		pq.swap(index, parentIndex)
+
+		//Increment
+		index = parentIndex
+		parentIndex = (index - 1) / 2
+	}
+}
+
+func (pq *PriorityQueue) swap(i, j int) {
+	pq.nodes[i], pq.nodes[j] = pq.nodes[j], pq.nodes[i]
+}
