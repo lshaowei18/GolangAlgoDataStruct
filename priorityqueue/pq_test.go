a93f7a152543f84a2e81b6cd17de69749d6514f4
Shao Wei <lshaowei18@gmail.com>
2019-03-20 22:43:49 +0800

    Wrote test for swap function

--- priorityqueue/pq_test.go
+++ priorityqueue/pq_test.go
@@ -30,3 +30,29 @@ func TestEnqueue(t *testing.T) {
 		}
 	}
 }
+
+var swapTests = []struct {
+	index1   int
+	index2   int
+	values   []Node
+	expected []Node
+}{
+	{
+		1,
+		3,
+		[]Node{{0, ""}, {1, ""}, {2, ""}, {3, ""}, {4, ""}},
+		[]Node{{0, ""}, {3, ""}, {2, ""}, {1, ""}, {4, ""}},
+	},
+}
+
+func TestSwap(t *testing.T) {
+	for _, tt := range swapTests {
+		pq := PriorityQueue{tt.values}
+		pq.swap(tt.index1, tt.index2)
+		actual := pq.nodes
+
+		if !reflect.DeepEqual(actual, tt.expected) {
+			t.Errorf("Want %v, got %v", tt.expected, actual)
+		}
+	}
+}
