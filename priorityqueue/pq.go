308c818dba517040a98f7841365bfe16b315bb4a
Shao Wei <lshaowei18@gmail.com>
2019-03-21 13:01:56 +0800

    Added Dequeue

--- priorityqueue/pq.go
+++ priorityqueue/pq.go
@@ -28,6 +28,47 @@ func (pq *PriorityQueue) Enqueue(priority int, value string) {
 	}
 }
 
+func (pq *PriorityQueue) Dequeue() Node {
+	//Returns empty node if pq is empty
+	if len(pq.nodes) == 0 {
+		return Node{}
+	}
+
+	dequeued := pq.nodes[0]
+
+	pq.nodes[0] = pq.nodes[len(pq.nodes)-1] //Replace last node with first node
+	pq.nodes = pq.nodes[:len(pq.nodes)-1]   //Remove last node
+
+	index := 0
+
+	for {
+		n := len(pq.nodes)
+
+		left := index*2 + 1
+		//Check if children are out of range, once left is out right will be too
+		if left >= n {
+			break
+		}
+		swap := left
+
+		// right is not out of range and has higher priority than left
+		if right := left + 1; right < n && !pq.lowerPriority(right, left) {
+			swap = right
+		}
+
+		//if index has higher priority than child, break
+		if !pq.lowerPriority(index, swap) {
+			break
+		}
+
+		pq.swap(index, swap)
+
+		index = swap
+	}
+
+	return dequeued
+}
+
 func (pq *PriorityQueue) swap(i, j int) {
 	pq.nodes[i], pq.nodes[j] = pq.nodes[j], pq.nodes[i]
 }
