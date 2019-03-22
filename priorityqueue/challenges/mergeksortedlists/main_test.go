59fd69281fe20e66183c5ebd14f14fb7cd0264c2
Shao Wei <lshaowei18@gmail.com>
2019-03-22 14:37:48 +0800

    Started IntToListNode test, check if it returns a pointer
    Wanted to dive straight into the problem, but I realize that I can't fully test
    my code if I can't turn the integers into ListNodes and the list of integers
    into ListNode Pointers.

--- priorityqueue/challenges/mergeksortedlists/main_test.go
+++ priorityqueue/challenges/mergeksortedlists/main_test.go
@@ -1,1 +1,16 @@
 package mergesortedlists
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIntToListNode(t *testing.T) {
+	want := 4
+	ln := IntToListNode(want)
+
+	//Ln should be a ListNode pointer
+	if reflect.ValueOf(ln).Kind() != reflect.Ptr {
+		t.Errorf("Should return a pointer, got %v instead", reflect.ValueOf(ln).Kind())
+	}
+}
