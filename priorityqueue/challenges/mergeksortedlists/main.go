62c732c7f346d054ce1a5640b086864d322d94b4
Shao Wei <lshaowei18@gmail.com>
2019-03-22 15:40:52 +0800

    Added IntSliceToSingly

--- priorityqueue/challenges/mergeksortedlists/main.go
+++ priorityqueue/challenges/mergeksortedlists/main.go
@@ -12,3 +12,15 @@ type PriorityQueue struct {
 func IntToListNode(n int) *ListNode {
 	return &ListNode{4, nil}
 }
+
+func IntSliceToSingly(s []int) *ListNode {
+	head := &ListNode{s[0], nil}
+
+	node := head
+	for i := 1; i < len(s); i++ {
+		newNode := &ListNode{s[i], nil}
+		node.Next = newNode
+		node = newNode
+	}
+	return head
+}
