bcd516dab9f1ddfb751ac700bae375a80f410a78
Shao Wei <lshaowei18@gmail.com>
2019-03-25 14:14:20 +0800

    Added FindMedian

--- priorityqueue/challenges/findmediandatastream/main.go
+++ priorityqueue/challenges/findmediandatastream/main.go
@@ -12,3 +12,17 @@ func Constructor() MedianFinder {
 func (this *MedianFinder) AddNum(num int) {
 	this.elements = append(this.elements, num)
 }
+
+func (this *MedianFinder) FindMedian() float64 {
+	n := len(this.elements)
+
+	i := (n - 1) / 2
+
+	if n%2 != 0 {
+		return float64(this.elements[i])
+	}
+
+	sum := float64(this.elements[i] + this.elements[i+1])
+
+	return sum / 2
+}
