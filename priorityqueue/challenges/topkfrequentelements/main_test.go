d3020eea9f71f2b66e0dfc4eca77997c3964a10d
Shao Wei <lshaowei18@gmail.com>
2019-03-25 11:45:22 +0800

    Added test for topkfrequent

---
+++ priorityqueue/challenges/topkfrequentelements/main_test.go
@@ -0,0 +1,29 @@
+package topkfrequent
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTopKFrequent(t *testing.T) {
+	tests := []struct {
+		input []int
+		k     int
+		want  []int
+	}{
+		{
+			[]int{1, 1, 1, 2, 2, 3, 3},
+			2,
+			[]int{1, 2},
+		},
+	}
+
+	for _, tt := range tests {
+
+		got := topKFrequent(tt.input, tt.k)
+
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("Want %v, got %v", tt.want, got)
+		}
+	}
+}
