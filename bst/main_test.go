ec0721a43ed491a8b11b3de3f28248527c60fd71
Shao Wei <lshaowei18@gmail.com>
2019-03-26 17:02:19 +0800

    Added Test for Insert

--- bst/main_test.go
+++ bst/main_test.go
@@ -1,1 +1,17 @@
 package bst
+
+import "testing"
+
+func TestInsert(t *testing.T) {
+	bst := BinarySearchTree{}
+
+	want := 10
+
+	bst.Insert(want)
+
+	got := bst.root.Val
+
+	if got != want {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+}
