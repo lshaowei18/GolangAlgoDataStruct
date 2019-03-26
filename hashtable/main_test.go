0f49f94b5a9e34ef05413635ec0a403356c49f45
Shao Wei <lshaowei18@gmail.com>
2019-03-26 09:56:13 +0800

    Added test for Hash

--- hashtable/main_test.go
+++ hashtable/main_test.go
@@ -1,1 +1,23 @@
 package hashtable
+
+import "testing"
+
+func TestHash(t *testing.T) {
+	tests := []struct {
+		key    string
+		length int
+		want   int
+	}{
+		{"pink", 10, 0},
+		{"orangered", 10, 7},
+		{"cyan", 10, 3},
+	}
+
+	for _, tt := range tests {
+		got := Hash(tt.key, tt.length)
+
+		if got != tt.want {
+			t.Errorf("Got %v, want %v", got, tt.want)
+		}
+	}
+}
