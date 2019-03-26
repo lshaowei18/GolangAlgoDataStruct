634e222158c902d128b606c27c9548ee668cdfc4
Shao Wei <lshaowei18@gmail.com>
2019-03-26 17:14:00 +0800

    Refactored, added new test case for Insert

--- bst/main_test.go
+++ bst/main_test.go
@@ -5,11 +5,19 @@ import "testing"
 func TestInsert(t *testing.T) {
 	bst := BinarySearchTree{}
 
-	want := 10
+	values := []int{10, 6, 15}
 
+	want := values[0]
 	bst.Insert(want)
-
 	got := bst.root.Val
+
+	if got != want {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+
+	want = values[1]
+	bst.Insert(want)
+	got = bst.root.Left.Val
 
 	if got != want {
 		t.Errorf("Got %v, want %v", got, want)
