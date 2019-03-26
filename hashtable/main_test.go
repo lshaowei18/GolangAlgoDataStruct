f5a900d072bb75b7fe757b0cd46dbaa6865e24ef
Shao Wei <lshaowei18@gmail.com>
2019-03-26 11:36:40 +0800

    Added test for Get

--- hashtable/main_test.go
+++ hashtable/main_test.go
@@ -63,3 +63,47 @@ func TestSet(t *testing.T) {
 		}
 	})
 }
+
+func TestGet(t *testing.T) {
+	tests := []struct {
+		name   string
+		values []string
+		key    string
+		want   string
+	}{
+		{
+			"Different hash keys",
+			[]string{"hello", "goodbye", "hi"},
+			"hello",
+			"hello",
+		},
+		{
+			"Invalid key",
+			[]string{"hello", "goodbye", "hi"},
+			"chicken",
+			"",
+		},
+		{
+			"Same hash keys",
+			[]string{"cyan", "goodbye"},
+			"cyan",
+			"cyan",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ht := HashTable{}
+
+			for _, v := range tt.values {
+				ht.Set(ht.Hash(v), v)
+			}
+
+			got := ht.Get(tt.key)
+
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
