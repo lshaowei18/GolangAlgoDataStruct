cb58c00e3cbc073c25c0c9f206367b77f617daba
Shao Wei <lshaowei18@gmail.com>
2019-03-26 10:35:57 +0800

    Changed test cases and fix bugs for Hash

--- hashtable/main_test.go
+++ hashtable/main_test.go
@@ -4,19 +4,18 @@ import "testing"
 
 func TestHash(t *testing.T) {
 	tests := []struct {
-		key    string
-		length int
-		want   int
+		key  string
+		want int
 	}{
-		{"hello", 13, 7},
-		{"goodbye", 13, 9},
-		{"cyan", 13, 5},
-		{"hi", 13, 10},
+		{"hello", 40},
+		{"goodbye", 23},
+		{"cyan", 23},
+		{"hi", 45},
 	}
 
 	for _, tt := range tests {
 		ht := HashTable{}
-		got := ht.Hash(tt.key, tt.length)
+		got := ht.Hash(tt.key)
 
 		if got != tt.want {
 			t.Errorf("Got %v, want %v", got, tt.want)
