71c6967cbb560c58e7b9eab08c136018c4513f86
Shao Wei <lshaowei18@gmail.com>
2019-03-26 10:24:29 +0800

    Updated Hash to pass test case

--- hashtable/main.go
+++ hashtable/main.go
@@ -1,10 +1,11 @@
 package hashtable
 
 func Hash(key string, length int) int {
+	PRIME := 31
 	total := 0
 	for _, c := range key {
 		v := int(c) - 96
-		total = (total + v) % length
+		total = (total*PRIME + v) % length
 	}
 
 	return total
