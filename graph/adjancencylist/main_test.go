07d678392d59feb6ae95d3c1c0f8e53d623106a8
Shao Wei <lshaowei18@gmail.com>
2019-04-07 01:04:43 +0800

    Added new test cases for AddVertex

--- graph/adjancencylist/main_test.go
+++ graph/adjancencylist/main_test.go
@@ -6,10 +6,23 @@ import (
 
 func TestAddVertex(t *testing.T) {
 	g := makeGraph()
-	want := "Tokyo"
-	g.addVertex(want)
 
-	if _, ok := g.AdjacencyList[want]; !ok {
-		t.Errorf("Key %s not added to AdjancencyList in graph.", want)
-	}
+	t.Run("Add unique vertex", func(t *testing.T) {
+		want := "Tokyo"
+		g.addVertex(want)
+
+		if _, ok := g.AdjacencyList[want]; !ok {
+			t.Errorf("Key %s not added to AdjancencyList in graph.", want)
+		}
+	})
+	t.Run("add non-unique vertex", func(t *testing.T) {
+		want := "Tokyo"
+		g.AdjacencyList[want] = append(g.AdjacencyList[want], "Hi")
+
+		g.addVertex(want)
+
+		if len(g.AdjacencyList[want]) == 0 {
+			t.Errorf("Repeated key should not replace existing list")
+		}
+	})
 }
