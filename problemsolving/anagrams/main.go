6543120a0c84f216bb527b9751b7ee83fbe205ea
Shao Wei <lshaowei18@gmail.com>
2019-06-05 13:25:10 +0800

    Added anagram and test

---
+++ problemsolving/anagrams/main.go
@@ -0,0 +1,30 @@
+package anagram
+
+func validAnagram(s1 string, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	if len(s1) == 0 && len(s2) == 0 {
+		return true
+	}
+
+	s1Map := make(map[string]int)
+	s2Map := make(map[string]int)
+
+	for _, c := range s1 {
+		s1Map[string(c)]++
+	}
+
+	for _, c := range s2 {
+		s2Map[string(c)]++
+	}
+
+	for k, count := range s1Map {
+		if s2Map[k] != count {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ problemsolving/anagrams/main_test.go
@@ -0,0 +1,41 @@
+package anagram
+
+import "testing"
+
+func TestAnagram(t *testing.T) {
+	tests := []struct {
+		s1       string
+		s2       string
+		expected bool
+	}{
+		{
+			"", "", true,
+		},
+		{
+			"aaz", "zza", false,
+		},
+		{
+			"anagram", "nagaram", true,
+		},
+		{
+			"rat", "car", false,
+		},
+		{
+			"qwerty", "qeywrt", true,
+		},
+		{
+			"qwerty", "qeyw", false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("First Attempt", func(t *testing.T) {
+			actual := validAnagram(tt.s1, tt.s2)
+
+			if actual != tt.expected {
+				t.Errorf("s1: %v, s2: %v, actual: %v, expected: %v",
+					tt.s1, tt.s2, actual, tt.expected)
+			}
+		})
+
+	}
+}
